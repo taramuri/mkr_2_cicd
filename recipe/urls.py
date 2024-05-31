5c586eef0dc58669e5ef2afa06d14174955cb927
taramuri <gaufrerer@gmail.com>
2024-05-31 17:29:01 +0300

    add page for recipe details

--- recipe/urls.py
+++ recipe/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.main, name='main'),
     path('category/<int:category_id>/', views.category_detail, name='category_detail'),
+    path('recipe/<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -11,3 +11,7 @@ def category_detail(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     recipes = Recipe.objects.filter(category=category)
     return render(request, 'category_detail.html', {'category': category, 'recipes': recipes})
+
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
