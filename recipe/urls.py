e6694a469e2b818af244ef3436e2ec614fcac727
taramuri <gaufrerer@gmail.com>
2024-05-31 17:04:51 +0300

    first commit

---
+++ recipe/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('category/<int:category_id>/', views.category_detail, name='category_detail'),
+]
